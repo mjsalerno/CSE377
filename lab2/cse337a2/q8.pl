ee97ddd93914373aa463bea0f02bddaf39bddb42
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-03-20 14:32:01 -0400

    fixed my extra credit

--- lab2/cse337a2/q8.pl
+++ lab2/cse337a2/q8.pl
@@ -5,7 +5,7 @@ use warnings;
 
 sub corse_title {
     my @c     = @{ $_[0] };
-    my $index = $_[1]-1;
+    my $index = $_[1] - 1;
     my $cname = $_[2];
 
     if ( !exists $c[$index]{$cname} ) {
@@ -16,13 +16,26 @@ sub corse_title {
     }
 }
 
-my %y1 = ( 114, "Computer Science I" );
-my %y2 = ( 220, "Systems-Level Programming" );
-my %y3 = ( 320, "Computer Organization and Architecture" );
+my %y1 = (
+    110, "Introduction to Computer Science",
+    114, "Computer Science I",
+    130, "Introduction to Programmingin C"
+);
+my %y2 = (
+    214, "Computer Science II", 219, "Computer Science III",
+    220, "Systems-Level Programming"
+);
+my %y3 = (
+    303, "Introduction to the Theory of Computation",
+    306, "Operating Systems",
+    320, "Computer Organization and Architecture"
+);
 
 my @classes = [ \%y1, \%y2, \%y3 ];
 
 print corse_title( @classes, 1, 114 ), "\n";
-print corse_title( @classes, 2, "220" ), "\n";
-print corse_title( @classes, 3, "320" ), "\n";
-print corse_title( @classes, 2, "214" ), "\n";
+print corse_title( @classes, 2, 220 ), "\n";
+print corse_title( @classes, 3, 320 ), "\n";
+print corse_title( @classes, 2, 214 ), "\n";
+print corse_title( @classes, 3, 306 ), "\n";
+print corse_title( @classes, 2, 306 ), "\n";
