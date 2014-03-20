2b094ed1728fa2f10a05b9537526c0282a9f773a
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-03-20 14:16:19 -0400

    deleted old code

--- lab2/cse337a2/q7.pl
+++ lab2/cse337a2/q7.pl
@@ -25,16 +25,6 @@ while ( my $line = <FILE> ) {
 	$count++;
 }
 
- # for my $key ( keys %names ) {
- #        my @value = $names{$key};
- #        print "$key => ";
-
- #        foreach my $x (@{$names{$key}}) {
- #        	print " $x";
- #        }
- #        print"\n";
- #    }
-
  for my $key ( keys %names ) {
         my @value = $names{$key};
         print "Group Number: $key\n";
