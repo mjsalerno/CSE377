0b5aa6f590f04fbe045f0d8430aacef358f75eac
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-03-28 22:16:34 -0400

    attempting the first assignment

---
+++ lab3/cse337a3/q1p2.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Enter the regex: ";
+my $regex = <>;
+chomp($regex);
+
+my @strings = ();
+my $in;
+
+print "Press ctrl-D to stop entering strings\n";
+print "Enter the strings to check\n";
+
+while ( $in = <> ) {
+	chomp($in);
+    push( @strings, $in );
+}
+
+print "\n";
+
+foreach my $str (@strings) {
+	if ($str =~ /$regex)/) {
+		print();
+	}
+}
